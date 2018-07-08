21afff0b26283b62091df86028e79980f8c2aa99
Jean-Francois Larouche <rouche@gmail.com>
2018-07-07 22:15:47 -0400

    Minify CSS

--- webpack-multi-setup/postcss.config.js
+++ webpack-multi-setup/postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     plugins: {
-        autoprefixer: {
+        'autoprefixer': {
             browsers: ['last 2 versions', 'iOS >= 8', 'IE >= 11']
-        }
+        },
+        'cssnano': {}
     }
 }
